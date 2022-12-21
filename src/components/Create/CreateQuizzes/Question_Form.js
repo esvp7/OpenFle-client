f2ab62d9f9aa2643099566d64a158427dc2c6b3c
unknown <you@example.com>
2022-12-21 06:59:28 -0500

    adding question functionalities

--- src/components/Create/CreateQuizzes/Question_Form.js
+++ src/components/Create/CreateQuizzes/Question_Form.js
@@ -25,13 +25,22 @@ import { Accordion,
   Typography,
   Select,
   MenuItem,
-  IconButton } from '@material-ui/core';
+  IconButton,
+  Switch,
+  Button } from '@material-ui/core';
 import { CropOriginal, 
   Subject,
   Radio, 
   CheckBox,
   ShortText,
-  Close } from "@material-ui/icons";
+  Close,
+  FilterNone, 
+  MoreVert,
+  AddCircleOutline,
+  OndemandVideo,
+  TextFields} from "@material-ui/icons";
+import { FcRightUp } from "react-icons/fc";
+import { BsTrash } from "react-icons/bs";
 
 const Question_Form = () => {
   const [questions, setQuestions] = useState(
@@ -47,18 +56,48 @@ open: true,
 required: false}]
 )
 
+function changeQuestion(text, i) {
+  var newQuestion = [...questions];
+  newQuestion[i].questionText = text;
+  setQuestions(newQuestion);
+  console.log(newQuestion);
+}
+
+function changeOptionValue(text, i, j) {
+  var optionsQuestion = [...questions];
+  optionsQuestion[i].options[j].optionText = text;
+  setQuestions(optionsQuestion);
+  console.log(optionsQuestion);
+}
+
+function addQuestionType(i, type) {
+  let qs = [...questions];
+  console.log(type)
+  qs[i].questionType = type;
+  setQuestions(qs);
+}
+
+function removeOption(i, j) {
+  var RemoveOptionQuestion = [...questions];
+  if (RemoveOptionQuestion[i].options.length > 1) {
+    RemoveOptionQuestion[i].options.splice(j, i);
+  setQuestions(RemoveOptionQuestion);
+  console.log(i + "__" + j)
+  }
+}
+
 const questionsUI = () => {
   return questions?.map((ques, i) => (
-    <div>
+    <div className="main-div">
       <Accordion expanded={questions[i].open} className={questions[i].open ? 'add_border' : ''}>
         <AccordionSummary
         aria-controls='panel1a-content'
         id="panel1a-header"
         elevation={1} style={{'width': '100%'}}>
-          { questions[i].open ? (
+          { !questions[i].open ? (
             <div>
               <Typography style={{'fontSize': '15px', 'fontWeight': '400', 
-              'letterSpacing': '.1px', 'lineHeight': '25px', 'paddingBottom': '8px'}}>
+              'letterSpacing': '.1px', 'paddingBottom': '8px'}}>
                 {i+1}. {questions[i].questionText}
               </Typography>
 
@@ -89,31 +128,71 @@ const questionsUI = () => {
               <input type="test"
                 className="question"
                 placeholder="Question"
-                value={ques.questionText}/>
+                value={ques.questionText}
+                onChange={(e) => {changeQuestion(e.target.value, i)}}/>
               <CropOriginal style={{'color': '#5F6368'}} />
                 <Select className="select" stlye={{'color': '#5F6368', 'fontSize': '13px'}}>
-                  <MenuItem id="text" value="Text"><Subject style={{'marginRight': '10px'}}/> Paragraph</MenuItem>
-                  <MenuItem id="checkbox" value="Checkbox"><CheckBox style={{'marginRight': '10px', 'color': '#70757a'}}/> Checkbox</MenuItem>
-                  <MenuItem id="radio" value="Radio"><Radio style={{'marginRight': '10px', 'color': '#70757a'}} checked/> Multiple Choice</MenuItem>
+                  <MenuItem id="text" value="Text" onClick={() => {addQuestionType(i, "text")}}>
+                    <Subject style={{'marginRight': '10px'}}/>Paragraph</MenuItem>
+                  <MenuItem id="checkbox" value="Checkbox" onClick={() => {addQuestionType(i, "checkbox")}}>
+                    <CheckBox style={{'marginRight': '10px', 'color': '#70757a'}}/> Checkbox</MenuItem>
+                  <MenuItem id="radio" value="Radio" onClick={() => {addQuestionType(i, "radio")}}>
+                    <Radio style={{'marginRight': '10px', 'color': '#70757a'}} checked/> Multiple Choice</MenuItem>
                 </Select>
             </div>
             {ques.options.map((op, j) => (
               <div className="add_question_body" key={j}>
                 {
-                  (ques.questionType="text") ? 
+                  (ques.questionType!=="text") ? 
                     <input type={ques.questionType} style={{'marginRight': '10px'}}/> :
                     <ShortText style={{'marginRight': '10px'}}/>
                 }
                 <div>
-                  <input type="text" className="text_input" placeholder="option" value={ques.options[j].ooptionText}/>
+                  <input type="text" 
+                  className="text_input" 
+                  placeholder="option" 
+                  value={ques.options[j].optionText}
+                  onChange={(e) => {changeOptionValue(e.target.value, i, j)}}/>
                 </div>
                 <CropOriginal style={{'color': '#5f6368'}} />
                 <IconButton aria-label="delete">
-                    <Close />
+                    <Close onClick={() => {removeOption(i, j)}} />
                 </IconButton>
               </div>
             ))}
+            <div className="add_footer">
+              <div className="add_question_bottom_left">
+                <Button size="small" style={{'textTransform': 'none', 'color': '#4285f4', 
+                'fontSize': '13px', 'fontWeight': '600'}}>
+                  <FcRightUp style={{'border': '2 px solid #4285f4', 'padding': '2px', 'marginRight': '4px'}}/>
+                Answer Key</Button>
+              </div>
+
+              <div className="add_question_bottom">
+
+                <IconButton aria-label="copy">
+                  <FilterNone />
+                </IconButton>
+
+                
+                <IconButton aria-label="delete">
+                  <BsTrash />
+                </IconButton>
+                  <span style={{'color': '#5f6368', 'fontSize': '13px'}}>Required</span>
+                  <Switch name="checkedA" color="primary" checked></Switch>
+                <IconButton>
+                  <MoreVert />
+                </IconButton>
+              </div>
+            </div>
           </AccordionDetails>
+
+          <div className="question_edit">
+              <AddCircleOutline className="edit" />
+              <OndemandVideo className="edit" />
+              <CropOriginal className="edit" />
+              <TextFields className="edit" />
+          </div>
         </div>
       </Accordion>
     </div>
@@ -129,10 +208,9 @@ const questionsUI = () => {
               style={{ 'color': 'black'}} placeholder="Untitled Quiz"/>
             <input type="text" className="question_form_top_desc" 
               style={{ 'color': 'black' }} placeholder="Quiz Description"/>
-
-              {questionsUI()}
           </div>
         </div>
+        {questionsUI()}
       </div>
     </div>
   )
